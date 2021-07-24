716ca572c921d0d67df0236c1d4db721e3c4785e
Matt Drake <drake.mattc@gmail.com>
2021-07-24 01:53:10 -0400

    Persist admin account across local datastore resets

--- utils/dbsetup.js
+++ utils/dbsetup.js
@@ -6,9 +6,12 @@ const { species } = require("../src/utils/species");
 const { breeds } = require("../src/utils/breeds");
 const { availability } = require("../src/utils/availability");
 const { dispositions } = require("../src/utils/dispositions");
+const { User } = require("../src/models/user");
+const { secrets } = require("../src/secrets");
 
 // The ID of your GCS bucket
-const bucketName = 'https://storage.googleapis.com/capstone-project-318221.appspot.com/';
+const bucketName =
+  "https://storage.googleapis.com/capstone-project-318221.appspot.com/";
 
 const datastore = new Datastore();
 
@@ -23,8 +26,11 @@ animal1.Name = "Bucky";
 animal1.Species = species.DOG;
 animal1.Breed = breeds.Dog.CAVALIER_KING_CHARLES_SPANIEL;
 animal1.Gender = gender.MALE;
-animal1.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal1.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal1.Age = "1 year";
 animal1.DateCreated = "2021-02-01";
 animal1.Availability = availability.AVAILABLE;
@@ -37,8 +43,11 @@ animal2.Name = "Peanut";
 animal2.Species = species.DOG;
 animal2.Breed = breeds.Dog.YORKSHIRE_TERRIER;
 animal2.Gender = gender.MALE;
-animal2.Disposition = [dispositions.SPECIAL_NEEDS, dispositions.ADULTS_ONLY,
-  dispositions.ANIMAL_MUST_BE_LEASHED_AT_ALL_TIMES];
+animal2.Disposition = [
+  dispositions.SPECIAL_NEEDS,
+  dispositions.ADULTS_ONLY,
+  dispositions.ANIMAL_MUST_BE_LEASHED_AT_ALL_TIMES,
+];
 animal2.Age = "6 years";
 animal2.DateCreated = "2021-06-01";
 animal2.Availability = availability.AVAILABLE;
@@ -50,8 +59,11 @@ animal3.Name = "Mustafa";
 animal3.Species = species.CAT;
 animal3.Breed = breeds.Cat.AMERICAN_CURL;
 animal3.Gender = gender.MALE;
-animal3.Disposition = [dispositions.SOLO_ANIMAL, dispositions.GOOD_WITH_CHILDREN,
-  dispositions.SPECIAL_DIET];
+animal3.Disposition = [
+  dispositions.SOLO_ANIMAL,
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.SPECIAL_DIET,
+];
 animal3.Age = "5 years";
 animal3.DateCreated = "2021-05-01";
 animal3.Availability = availability.AVAILABLE;
@@ -75,8 +87,12 @@ animal5.Name = "Ziva";
 animal5.Species = species.DOG;
 animal5.Breed = breeds.Dog.AUSTRALIAN_SHEPHERD;
 animal5.Gender = gender.FEMALE;
-animal5.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.SPECIAL_DIET,
-  dispositions.ENERGETIC, dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal5.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.SPECIAL_DIET,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal5.Age = "7 years";
 animal5.DateCreated = "2021-04-01";
 animal5.Availability = availability.PENDING;
@@ -88,8 +104,11 @@ animal6.Name = "Dobby";
 animal6.Species = species.DOG;
 animal6.Breed = breeds.Dog.BEAGLE;
 animal6.Gender = gender.FEMALE;
-animal6.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal6.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal6.Age = "4 years";
 animal6.DateCreated = "2020-07-01";
 animal6.Availability = availability.AVAILABLE;
@@ -173,7 +192,10 @@ animal13.Name = "Nala";
 animal13.Species = species.CAT;
 animal13.Breed = breeds.Cat.BENGAL;
 animal13.Gender = gender.FEMALE;
-animal13.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.NEEDS_COMPANION];
+animal13.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.NEEDS_COMPANION,
+];
 animal13.Age = "9 years";
 animal13.DateCreated = "2020-09-02";
 animal13.Availability = availability.AVAILABLE;
@@ -209,7 +231,10 @@ animal16.Name = "Geico";
 animal16.Species = species.OTHER;
 animal16.Breed = breeds.Other.COMMON_LEOPARD_GECKO;
 animal16.Gender = gender.MALE;
-animal16.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC];
+animal16.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+];
 animal16.Age = "1 year";
 animal16.DateCreated = "2021-07-01";
 animal16.Availability = availability.AVAILABLE;
@@ -221,8 +246,11 @@ animal17.Name = "Fido";
 animal17.Species = species.DOG;
 animal17.Breed = breeds.Dog.LABRADOR;
 animal17.Gender = gender.MALE;
-animal17.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal17.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal17.Age = "5 years";
 animal17.DateCreated = "2021-07-03";
 animal17.Availability = availability.AVAILABLE;
@@ -234,8 +262,11 @@ animal18.Name = "Lady";
 animal18.Species = species.DOG;
 animal18.Breed = breeds.Dog.CAVALIER_KING_CHARLES_SPANIEL;
 animal18.Gender = gender.FEMALE;
-animal18.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal18.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal18.Age = "5 years";
 animal18.DateCreated = "2021-06-02";
 animal18.Availability = availability.AVAILABLE;
@@ -247,8 +278,11 @@ animal19.Name = "Tramp";
 animal19.Species = species.DOG;
 animal19.Breed = breeds.Dog.LABRADOR;
 animal19.Gender = gender.MALE;
-animal19.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal19.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal19.Age = "5 years";
 animal19.DateCreated = "2021-05-02";
 animal19.Availability = availability.AVAILABLE;
@@ -260,8 +294,11 @@ animal20.Name = "Pongo";
 animal20.Species = species.DOG;
 animal20.Breed = breeds.Dog.LABRADOR;
 animal20.Gender = gender.MALE;
-animal20.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal20.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal20.Age = "5 years";
 animal20.DateCreated = "2021-04-02";
 animal20.Availability = availability.AVAILABLE;
@@ -274,8 +311,11 @@ animal21.Name = "Lassie";
 animal21.Species = species.DOG;
 animal21.Breed = breeds.Dog.LABRADOR;
 animal21.Gender = gender.FEMALE;
-animal21.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal21.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal21.Age = "5 weeks";
 animal21.DateCreated = "2021-03-02";
 animal21.Availability = availability.AVAILABLE;
@@ -287,8 +327,11 @@ animal22.Name = "Beethoven";
 animal22.Species = species.DOG;
 animal22.Breed = breeds.Dog.LABRADOR;
 animal22.Gender = gender.MALE;
-animal22.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal22.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal22.Age = "5 years";
 animal22.DateCreated = "2021-02-10";
 animal22.Availability = availability.AVAILABLE;
@@ -300,8 +343,11 @@ animal23.Name = "Benji";
 animal23.Species = species.DOG;
 animal23.Breed = breeds.Dog.FRENCH_BULLDOG;
 animal23.Gender = gender.MALE;
-animal23.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal23.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal23.Age = "5 years";
 animal23.DateCreated = "2021-01-01";
 animal23.Availability = availability.AVAILABLE;
@@ -313,8 +359,11 @@ animal24.Name = "Clifford";
 animal24.Species = species.DOG;
 animal24.Breed = breeds.Dog.PUG;
 animal24.Gender = gender.MALE;
-animal24.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal24.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal24.Age = "5 years";
 animal24.DateCreated = "2021-07-02";
 animal24.Availability = availability.AVAILABLE;
@@ -326,8 +375,11 @@ animal25.Name = "Old Yeller";
 animal25.Species = species.DOG;
 animal25.Breed = breeds.Dog.GERMAN_SHEPHERD;
 animal25.Gender = gender.MALE;
-animal25.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal25.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal25.Age = "5 years";
 animal25.DateCreated = "2020-12-01";
 animal25.Availability = availability.AVAILABLE;
@@ -339,8 +391,11 @@ animal26.Name = "Eddie Crane";
 animal26.Species = species.DOG;
 animal26.Breed = breeds.Dog.POODLE;
 animal26.Gender = gender.MALE;
-animal26.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal26.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal26.Age = "5 years";
 animal26.DateCreated = "2020-11-01";
 animal26.Availability = availability.AVAILABLE;
@@ -352,8 +407,11 @@ animal27.Name = "Perdita";
 animal27.Species = species.DOG;
 animal27.Breed = breeds.Dog.SIBERIAN_HUSKY;
 animal27.Gender = gender.FEMALE;
-animal27.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal27.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal27.Age = "5 years";
 animal27.DateCreated = "2020-10-01";
 animal27.Availability = availability.AVAILABLE;
@@ -365,8 +423,11 @@ animal28.Name = "Comet";
 animal28.Species = species.DOG;
 animal28.Breed = breeds.Dog.GOLDEN_RETREIVER;
 animal28.Gender = gender.MALE;
-animal28.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal28.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal28.Age = "5 years";
 animal28.DateCreated = "2020-09-01";
 animal28.Availability = availability.AVAILABLE;
@@ -378,8 +439,11 @@ animal29.Name = "Monty";
 animal29.Species = species.DOG;
 animal29.Breed = breeds.Dog.CORGI;
 animal29.Gender = gender.MALE;
-animal29.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal29.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal29.Age = "5 years";
 animal29.DateCreated = "2020-08-02";
 animal29.Availability = availability.AVAILABLE;
@@ -391,8 +455,11 @@ animal30.Name = "Scooby-Doo";
 animal30.Species = species.DOG;
 animal30.Breed = breeds.Dog.BORDER_COLLIE;
 animal30.Gender = gender.MALE;
-animal30.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal30.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal30.Age = "5 years";
 animal30.DateCreated = "2020-07-02";
 animal30.Availability = availability.AVAILABLE;
@@ -404,8 +471,11 @@ animal31.Name = "Snoopy";
 animal31.Species = species.DOG;
 animal31.Breed = breeds.Dog.BEAGLE;
 animal31.Gender = gender.MALE;
-animal31.Disposition = [dispositions.GOOD_WITH_CHILDREN, dispositions.ENERGETIC,
-  dispositions.GOOD_WITH_OTHER_ANIMALS];
+animal31.Disposition = [
+  dispositions.GOOD_WITH_CHILDREN,
+  dispositions.ENERGETIC,
+  dispositions.GOOD_WITH_OTHER_ANIMALS,
+];
 animal31.Age = "5 years";
 animal31.DateCreated = "2020-06-01";
 animal31.Availability = availability.AVAILABLE;
@@ -424,9 +494,10 @@ const user1 = {
 };
 saveUser(user1);
 
-const user2 = {
-  name: "John Doe",
-  email: "johndoe@gmail.com",
-  password: "asdfoweifjwoiefawpeoviaweoviajewovijwpovij",
+const buildUser2 = async () => {
+  const user2 = new User();
+  user2.setEmail("admin@shelter.com");
+  await user2.setPassword(secrets.ADMIN_PASSWORD);
+  user2.save();
 };
-saveUser(user2);
+buildUser2();
